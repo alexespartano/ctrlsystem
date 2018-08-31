c70815145abac79bd673c1c3ed7382632a8e02b4
Gilberto <riuku530@gmail.com>
2018-08-30 23:05:14 -0500

    actualizacion1

--- rec.php
+++ rec.php
@@ -14,58 +14,75 @@ if($div != "IT"){
 ?>
 <!doctype html>
 <html>
+<meta charset="utf-8"/>    
+    <meta name="viewport" content="width=device-width, initial-scale=1" />      
+    <link rel="shortcut icon" href="//www.ibm.com/favicon.ico" />
+    <meta name="geo.country" content="US" />  
+    <title>Assets</title>
+    
+    <script src="//1.www.s81c.com/common/stats/ida_stats.js"></script>
+    <link href="//1.www.s81c.com/common/v18/css/www.css" rel="stylesheet" />
+    <script src="//1.www.s81c.com/common/v18/js/www.js"></script>
+    <link href="//1.www.s81c.com/common/v18/css/forms.css" rel="stylesheet">
+<script src="//1.www.s81c.com/common/v18/js/forms.js"></script>
+    <link href="https://1.www.s81c.com/common/v18/css/tables.css" rel="stylesheet">
+    <script src="https://1.www.s81c.com/common/v18/js/tables.js"></script>
+    <script script type="text/javascript" src="js/filters2.js"></script>
+  <link href="https://1.www.s81c.com/common/v18/css/grid-fluid.css" rel="stylesheet">
+  <script>
+    IBMCore.common.util.config.set({
+       backtotop: {
+        enabled: true
+        }
+    });
+</script>   
+  </head>
+  <body id="ibm-com" class="ibm-type">
+    <div id="ibm-top" class="ibm-landing-page">
+<nav role="navigation" aria-label="NAV">
+        <div class="ibm-sitenav-menu-container">
+            <div class="ibm-sitenav-menu-name">
+                <div id="ibm-home"><a href="main.php">IBM®</a></div>
+                <a href="main.php">&nbsp;&nbsp;&nbsp;&nbsp;CTRLSYSTEM</a></div>
+            <div class="ibm-sitenav-menu-list">
+                <ul role="menubar">
+                    <li role="presentation" class="ibm-haschildlist"><button role="menuitem">Ctrl of Assets</button>
+                        <ul role="menu" aria-label="Assets">
+                            <li role="presentation"><a role="menuitem" href="assets.php">View/Modify</a></li>
+                            <li role="presentation"><a role="menuitem" href="peradd.php">Add</a></li>
+                            <li role="presentation"><a role="menuitem" href="perdel.php">Delete</a></li>
+                        </ul>
+                    </li>
+                    <li role="presentation"><a role="menuitem" href="rec.php">Records</a></li>
+                    <li role="presentation"><a role="menuitem"  href="service.php">Services</a></li>
+                    <li role="presentation"><a role="menuitem" href="mttos.php">Calendar of Maintenance</a></li>
+                     <li role="presentation"><a role="menuitem" href="tools.php">Tools</a></li>
+                      <li role="presentation"><a role="menuitem" href="TPMVIEWER.php">TPM</a></li>
+                       <li role="presentation"><a role="menuitem" href="spareparts.php">Spare Parts</a></li>
+                    <!-- Optional right side CTA link -->
+                    <li class="ibm-sitenav-menu-item-right">
+                      <p class="ibm-ind-link ibm-icononly ibm-icononly" style="margin-top: 7px;"><a class="ibm-profile-link"></a></p>
+                      <ul role="menu" style="margin-top: -15px;">
+                            <li role="presentation"><a role="menuitem"><?php echo" " .strtoupper($_SESSION['username'])."  ";?></a></li>
+                            <li role="presentation"><a role="menuitem" href="logout.php">Logout</a></li>
+                        </ul>
+                        
+                      </li>
+                </ul>
+            </div>
+             
+        </div>
+    </nav>
+    
 
-
-<head>
-<meta charset="utf-8">
-<title>Records</title>
- <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script script type="text/javascript" src="js/filters.js"></script>
-   <script script type="text/javascript" src="js/totop.js"></script>
-  <link rel="stylesheet" href="css/main.css" />
-</head>
-
-
-<body background="img/Background-Picture-Html.jpg">
-
-<!--NAVMENU-->
-<div class="row" id="header">
-<nav class="navbar navbar-inverse" id="nav">
-        <button class="navbar-toggle" data-toggle="collapse" data-target="#a" id="toggle">
-            ☰
-        </button>
-        <div class="navbar-header">
-        	<a class=" navbar-brand"><img src="img/ibm-logo-png-transparent-background.png" width="50" height="20"></a>
-            <a href="main.php" class="navbar-brand" id="title" title="Desarrollado por Alejandro Romero Aldrete">CTRL SYSTEM</a>
-        </div>       
-        <div class="collapse navbar-collapse" id="a">
-            <ul class="nav navbar-nav">
-                <li><a href="main.php">HOME</a></li>
-                 <li class="dropdown">
-                    <a href="assets.php" class="dropdown-toggle" data-toggle="dropdown" role="button">CTRL OF ASSETS</a>
-                    <ul class="dropdown-menu">
-                    	<li><a href="assets.php">VIEW/MODIFY</a></li>
-                        <li><a href="peradd.php">ADD</a></li>
-                        <li><a href="perdel.php">DELETE</a></li>
-                    </ul>
-                </li>
-                 <li id="highlightbox"><a href="rec.php">RECORDS</a></li>
-                 <li><a href="service.php">SERVICES</a></li>
-                 <li><a href="mttos.php">CALENDAR OF MAINTENANCE</a></li>
-                <li><a href="tools.php">TOOLS</a></li>
-                <li><a href="TPMVIEWER.php">TPM</a></li>
-                 <li><a href="spareparts.php">SPARE PARTS</a></li>
-            </ul>
             <ul class="nav navbar-nav pull-right">
             	<li><a><i class="glyphicon glyphicon-user"></i><?php echo" " .strtoupper($_SESSION['username'])."  ";?></a></li>
 				<li><a href='logout.php'>Logout</a></li>	            </ul>    
         </div>
     </nav>
 </div><!--ENDNAVMENU-->
+
+
 
 <!--FILTERS-->
 <form action="#">
@@ -75,8 +92,15 @@ if($div != "IT"){
             <br>
             <br>
             <br>
-                <select id="Input2" placeholder="TYPE" title="SEARCH FOR TYPE"  style="font-size: 9px;">
-  <option value="">TYPE</option>
+
+
+              <div class="ibm-columns">
+    <div class="ibm-col-6-4  ibm-nospacing">
+        <form class="ibm-row-form">
+<select id="Input2" title="SEARCH FOR TYPE">
+  <option value="" selected="selected">Type</option>
+
+                
                 <?php
                    require_once('connectsys.php');
                     $query = "SELECT TYPE FROM CTRLSYSTEM.REC GROUP BY TYPE";
@@ -96,8 +120,12 @@ if($div != "IT"){
         </div>
         <div class="col-sm-2">
             
+
+
              <select id="Input3" placeholder="AREA" title="SEARCH FOR AREA"  style="font-size: 9px;">
              <option value="">AREA</option>
+
+             
                                  <?php
                     require_once('connectsys.php');
                     $query = "SELECT AREA FROM CTRLSYSTEM.REC GROUP BY AREA";
