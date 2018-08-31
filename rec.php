9570997ae1ac204e84938a982aeb9e91dc7db2ad
Gilberto <riuku530@gmail.com>
2018-08-31 14:16:59 -0500

    rec.php finished

--- rec.php
+++ rec.php
@@ -12,13 +12,14 @@ if($div != "IT"){
 }
 }
 ?>
-<!doctype html>
-<html>
-<meta charset="utf-8"/>    
+<!DOCTYPE html>
+<html lang="en-US" >
+<head>
+    <meta charset="utf-8"/>    
     <meta name="viewport" content="width=device-width, initial-scale=1" />      
     <link rel="shortcut icon" href="//www.ibm.com/favicon.ico" />
     <meta name="geo.country" content="US" />  
-    <title>Assets</title>
+    <title>Records</title>
     
     <script src="//1.www.s81c.com/common/stats/ida_stats.js"></script>
     <link href="//1.www.s81c.com/common/v18/css/www.css" rel="stylesheet" />
@@ -27,7 +28,7 @@ if($div != "IT"){
 <script src="//1.www.s81c.com/common/v18/js/forms.js"></script>
     <link href="https://1.www.s81c.com/common/v18/css/tables.css" rel="stylesheet">
     <script src="https://1.www.s81c.com/common/v18/js/tables.js"></script>
-    <script script type="text/javascript" src="js/filters2.js"></script>
+    <script script type="text/javascript" src="js/filters.js"></script>
   <link href="https://1.www.s81c.com/common/v18/css/grid-fluid.css" rel="stylesheet">
   <script>
     IBMCore.common.util.config.set({
@@ -73,60 +74,23 @@ if($div != "IT"){
              
         </div>
     </nav>
-    
-
-            <ul class="nav navbar-nav pull-right">
-            	<li><a><i class="glyphicon glyphicon-user"></i><?php echo" " .strtoupper($_SESSION['username'])."  ";?></a></li>
-				<li><a href='logout.php'>Logout</a></li>	            </ul>    
-        </div>
-    </nav>
-</div><!--ENDNAVMENU-->
+        <!--filters-->      
+        <main role="main" aria-labelledby="ibm-pagetitle-h1">
+          <div id="ibm-pcon">
+            <div id="ibm-content">
+              <div id="ibm-content-body">
+                <div id="ibm-content-main"> 
 
 
-
-<!--FILTERS-->
-<form action="#">
-    <div class="row" id="FILTERSBOX">
-    	<div class="col-sm-2">
-    		</label><input type="text" id="Input1" placeholder="S/N" title="SEARCH FOR S/N"  style="font-size: 9px;" autofocus>
-            <br>
-            <br>
-            <br>
-
-
-              <div class="ibm-columns">
-    <div class="ibm-col-6-4  ibm-nospacing">
-        <form class="ibm-row-form">
-<select id="Input2" title="SEARCH FOR TYPE">
-  <option value="" selected="selected">Type</option>
-
-                
-                <?php
-                   require_once('connectsys.php');
-                    $query = "SELECT TYPE FROM CTRLSYSTEM.REC GROUP BY TYPE";
-                    $stmt = db2_prepare($db2, $query);
-                    if($stmt){
-                        $result = db2_execute($stmt);
-                        if (!$result) {
-                             echo "exec errormsg: " .db2_stmt_errormsg($stmt);
-                            exit;
-                          }
-                while($row = db2_fetch_array($stmt)){
-                    echo '<option value="'. $row[0] .'">'. $row[0] .'</option>';
-                    }   
-                }
-                    ?>
-                    </select>
-        </div>
-        <div class="col-sm-2">
-            
-
-
-             <select id="Input3" placeholder="AREA" title="SEARCH FOR AREA"  style="font-size: 9px;">
-             <option value="">AREA</option>
-
-             
-                                 <?php
+<div class="ibm-columns ibm-seamless ibm-padding-bottom-0" data-widget="setsameheight" data-items=".ibm-blocklink">
+    <div class="ibm-col-1-1 ibm-nospacing">
+        <form action="#" class="ibm-row-form">
+          <p>
+                    <input type="text" size="25" placeholder="Serial Number" id="Input1" autofocus>
+          &nbsp;&nbsp;&nbsp;
+                <select id="Input3" title="SEARCH FOR AREA">
+                <option value="" selected="selected">Area</option>
+     <?php
                     require_once('connectsys.php');
                     $query = "SELECT AREA FROM CTRLSYSTEM.REC GROUP BY AREA";
                     $stmt = db2_prepare($db2, $query);
@@ -141,33 +105,14 @@ if($div != "IT"){
                     }   
                 }
                     ?>
-                </select>
-            <br>
-            <br>
-            <br>
-<select id="Input4" placeholder="BRAND" title="SEARCH FOR BRAND"  style="font-size: 9px;">
-             <option value="">BRAND</option>
-                  <?php
-                    require_once('connectsys.php');
-                    $query = "SELECT BRAND FROM CTRLSYSTEM.REC GROUP BY BRAND";
-                    $stmt = db2_prepare($db2, $query);
-                    if($stmt){
-                        $result = db2_execute($stmt);
-                        if (!$result) {
-                             echo "exec errormsg: " .db2_stmt_errormsg($stmt);
-                            exit;
-                          }
-                while($row = db2_fetch_array($stmt)){
-                    echo '<option value="'. $row[0] .'">'. $row[0] .'</option>';
-                    }   
-                }
-                    ?>
+
+
 </select>
-    	</div>
-        <div class="col-sm-2">
-<select  id="Input5" placeholder="LOCATION" title="SEARCH FOR LOCATION"  style="font-size: 9px;">
- <option value="">LOCATION</option>
-                  <?php
+  &nbsp;&nbsp;&nbsp;&nbsp;
+<select id="Input5" title="SEARCH FOR LOCATION">
+  <option value="" selected="selected">Location</option>
+  <?php
+                
                     require_once('connectsys.php');
                     $query = 'SELECT "LOCATION" FROM CTRLSYSTEM.REC GROUP BY "LOCATION"';
                     $stmt = db2_prepare($db2, $query);
@@ -183,22 +128,63 @@ if($div != "IT"){
                 }
                     ?>
 </select>
-            <br>
-            <br>
-            <br>
-<select  id="Input6" placeholder="ING" title="SEARCH FOR ING."  style="font-size: 9px;">
-            <option value="">ING RESPO.</option>
-            <option value="ING1">ING1</option>
-            <option value="ING2">ING2</option>
-            <option value="ING3">ING3</option>
-            <option value="N/A">N/A</option>
-        </select>
-    	</div>
-        <div class="col-sm-2">
-        </label>
-<select  id="Input7" placeholder="DATE YY/MM/DD" title="SEARCH FOR DATE"  style="font-size: 9px;">
- <option value="">DATE YY/MM/DD</option>
-                  <?php
+            </p>
+        </form>
+    </div>
+    <div class="ibm-columns">
+    <div class="ibm-col-6-4  ibm-nospacing">
+        <form class="ibm-row-form">
+<select id="Input2" title="SEARCH FOR TYPE">
+  <option value="" selected="selected">Type</option>
+  <?php
+                    require_once('connectsys.php');
+                    $query = "SELECT TYPE FROM CTRLSYSTEM.INV GROUP BY TYPE";
+                    $stmt = db2_prepare($db2, $query);
+                    if($stmt){
+                        $result = db2_execute($stmt);
+                        if (!$result) {
+                             echo "exec errormsg: " .db2_stmt_errormsg($stmt);
+                            exit;
+                          }
+                while($row = db2_fetch_array($stmt)){
+                    echo '<option value="'. $row[0] .'">'. $row[0] .'</option>';
+                    }   
+                }
+    ?>
+</select>
+ &nbsp;&nbsp;&nbsp;&nbsp;
+<select  id="Input4" title="SEARCH FOR BRAND">
+  <option value="" selected="selected">Brand</option>
+  <?php
+                    require_once('connectsys.php');
+                    $query = "SELECT BRAND FROM CTRLSYSTEM.REC GROUP BY BRAND";
+                    $stmt = db2_prepare($db2, $query);
+                    if($stmt){
+                        $result = db2_execute($stmt);
+                        if (!$result) {
+                             echo "exec errormsg: " .db2_stmt_errormsg($stmt);
+                            exit;
+                          }
+                while($row = db2_fetch_array($stmt)){
+                    echo '<option value="'. $row[0] .'">'. $row[0] .'</option>';
+                    }   
+                }
+                    ?>
+</select>
+
+
+ &nbsp;&nbsp;&nbsp;&nbsp;
+<select id="Input6" title="SEARCH FOR ING RESPONSIBLE">
+  <option value="" selected="selected">Ing</option>
+  <option value="">N/A</option>
+  <option value="ING1">ING1</option>
+  <option value="ING2">ING2</option>
+  <option value="ING3">ING3</option>
+</select>
+ &nbsp;&nbsp;&nbsp;&nbsp;
+<select  id="Input7" title="SEARCH FOR DATE">
+  <option value="" selected="selected">Date</option>
+   <?php
                     require_once('connectsys.php');
                     $query = 'SELECT "DATE" FROM CTRLSYSTEM.REC GROUP BY "DATE"';
                     $stmt = db2_prepare($db2, $query);
@@ -214,25 +200,25 @@ if($div != "IT"){
                 }
                     ?>
 </select>
-        <br>
-        <br>
-         </div>
-         <div class="col-sm-3">
-         <p align="center"><button onClick="window.location.reload()" id="submitbutton" title="Refresh all the web Page and update sql"><i class="glyphicon glyphicon-refresh"></i>REFRESH</button><button onclick="clear(this.form);"  id="submitbutton" type="reset" title="Clear filters"><i class="glyphicon glyphicon-trash"></i>Clear</button>    <button onclick="filter()"  id="submitbutton" title="Execute filters"><i class="glyphicon glyphicon-search"></i>Submit</button></p> 
-          <!--COUNTER-->
-<div class="row">
-    <div class="col-sm-5"></div>
-    <div class="col-sm-1" style=" color:rgb(204,204,204);">Items:<p id="con"></p></div>
-</div><!--COUNTER--> 
-    	</div>
-    </div></form><!--ENDFILTERS-->
-<!--CONTAINER-->
-<div class="row" id="tablecontainer">
-	<div class="col-sm-12">
-		<table class="table table-bordered" id="myTable">
-			<thead>
-				<tr>    	
-					<th>LOCATION</th>
+</div> 
+
+<div class="ibm-col-6-2  ibm-nospacing">
+    <span class="ibm-ind-link ibm-btn-row"><a class="ibm-refresh-link ibm-btn-sec ibm-btn-blue-50" href="#" onClick="window.location.reload()">Refresh</a><a class="ibm-search-link ibm-btn-sec ibm-btn-blue-50" href="#" onclick="filter()">
+    Search</a><a class="ibm-calculator-link ibm-btn-sec ibm-btn-blue-50">Items:<b id="con"></b></a></span>
+</div>
+</form>
+</div><!--ENDFILTERS-->
+<br>
+<br>
+<br>
+<br>
+<!--CONTENT-->
+<div class="ibm-columns ibm-seamless ibm-padding-bottom-0" data-widget="setsameheight" data-items=".ibm-blocklink">
+    <div class="ibm-col-1-1 ibm-nospacing">
+    <table class="ibm-data-table ibm-grid ibm-altrows  ibm-padding-small ibm-center" id="myTable" data-scrollaxis="x" cellspacing="0" cellpadding="0" border="0" >
+            <thead>
+                <tr>
+                        <th>LOCATION</th>
                     <th>AREA</th>
                     <th>BRAND</th>
                     <th>TYPE</th>
@@ -242,28 +228,36 @@ if($div != "IT"){
                     <th>ING</th>
                     <th>DATE</th>
                     <th>DESCRIPTION</th>
-                    <th title="Cleaning">PHYS. VERI.</th>
+                    <th title="Cleaning"  >PHYS. VERI.</th>
                     <th title="check software/Updates.">UPDATES</th>
-				</tr>
-			</thead>
-            <?php
-            $PHYS = '<i class="glyphicon glyphicon-ok"></i>';
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
+
+            $PHYS = '<a class="ibm-confirm-link"></a>';
+
             require_once('connectsys.php');
             $query = 'SELECT "LOCATION", "AREA", "BRAND", "TYPE", "SN","MT","MODEL", "ING", "DATE", "DIVISION", "DESCRI","UPDATES" FROM CTRLSYSTEM.REC';
                 $stmt = db2_prepare($db2, $query);
-			if($stmt){
+      if($stmt){
                  $result = db2_execute($stmt);
                         if (!$result) {
                              echo "exec errormsg: " .db2_stmt_errormsg($stmt);
                             exit;
                           }
         while($row = db2_fetch_array($stmt)){
+            
             if($row[11] == 1){
-                $update ='<i class="glyphicon glyphicon-ok"></i>';
-            }else{
-                $update = "N/A";
+                $update = '<a class="ibm-confirm-link"></a>';
+
+            }else{ 
+                $update = "N/A" ;
             }
-            echo '<tr><td align="center">' .
+
+            echo '<tr><td align="center" >' .
+
             $row[0] . '</td><td align="center">' .
             $row[1] . '</td><td align="center">' .
             $row[2] . '</td><td align="center">' .
@@ -273,18 +267,70 @@ if($div != "IT"){
             $row[6] . '</td><td align="center">' .
             $row[7] . '</td><td align="center">' .
             $row[8] . '</td><td align="center">' .
-            $row[10] . '</td><td align="center">'.
-            $PHYS . '</td><td align="center">'.
+            $row[10] . '</td><td align="center" class="ibm-ind-link"> '.
+            $PHYS . '</td><td align="center" class="ibm-ind-link">'.
             $update . '</td>';
             echo '</tr>';
-            }	
+            } 
             echo '</table>';
         }
             db2_close($db2);
             ?>
-             <a href="#" class="scrollup"><p align="center"><button class="btn btn-default btn-lg" style="background-color:rgb(150,150,150);"><i class="glyphicon glyphicon-chevron-up"></i></button></p></a>
-		</table>
-	</div>
-</div><!--ENDCONATINER-->
-</body>
+            </tbody>
+        </table>
+    </div>
+</div>
+<br>
+<br>
+<br>
+<br><br>
+<br>
+<br>
+<br><br>
+<br>
+<br>
+<br><br>
+<br>
+<br>
+<br><br>
+<br>
+<br>
+<br><br>
+<br>
+<br>
+<br><br>
+<br>
+<br>
+<br><br><br>
+<br>
+<br>
+<br><br>
+<br>
+<br>
+<br><br>
+<br>
+<br>
+<br><br><br>
+<br>
+<br>
+<br><br>
+<br>
+<br>
+<br><br>
+<br>
+<br>
+<br>
+<br>
+
+
+              </div>
+            </div>
+          </div>
+        </main>
+<script>
+function modi(id){
+    win2 = window.open('mod.php?id='+id,'Modifica_U','scrollbars=no,top=220,left=500,width=580,height=450');    
+}
+</script>
+  </body>
 </html>
