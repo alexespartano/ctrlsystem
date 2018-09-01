5dbc5af467e59ee2c19cc0f8484498e646e702c4
Alejandro Romero Aldrete <alejandro_romero_aldrete@hotmail.com>
2018-08-31 22:26:00 -0500

    tools.php finished

--- TPMDETAILS.php
+++ TPMDETAILS.php
@@ -36,7 +36,6 @@ if($user == "GERENCIA"){
   <link href="https://1.www.s81c.com/common/v18/css/grid-fluid.css" rel="stylesheet">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script>
     IBMCore.common.util.config.set({
        backtotop: {
