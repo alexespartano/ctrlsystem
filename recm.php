c61d84780eec0ea09f1888a1e2de7a5d9ebb9f40
Gilberto <riuku530@gmail.com>
2018-09-05 14:54:52 -0500

    finishi of recm.php and viewermfs.php

--- recm.php
+++ recm.php
@@ -1,7 +1,5 @@
 <?php
-/*Desarrollado por Alejandro Romero Aldrete y Gilberto Bustamante Sanchez*/
-session_start();
-// If session variable is not set it will redirect to login page
+/*Desarrollado por Alejandro Romero Aldrete*/
 session_start();
 // If session variable is not set it will redirect to login page
 if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
@@ -14,57 +12,97 @@ if($div != "MFS"){
 }
 }
 ?>
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en-US" >
 <head>
-<meta charset="utf-8">
-<title>Records</title>
- <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script script type="text/javascript" src="js/filters.js"></script>
-   <script script type="text/javascript" src="js/totop.js"></script>
-  <link rel="stylesheet" href="css/main.css" />
-</head>
-<body background="img/Background-Picture-Html.jpg">
-<!--NAVMENU-->
-<div class="row" id="header">
-<nav class="navbar navbar-inverse" id="nav">
-        <button class="navbar-toggle" data-toggle="collapse" data-target="#a" id="toggle">
-            ☰
-        </button>
-        <div class="navbar-header">
-            <a class=" navbar-brand"><img src="img/ibm-logo-png-transparent-background.png" width="50" height="20"></a>
-            <a href="viewermfs.php" class="navbar-brand" id="title" title="Desarrollado por Alejandro Romero Aldrete y Gilberto Bustamante Sanchez">CTRL SYSTEM</a>
-        </div>       
-        <div class="collapse navbar-collapse" id="a">
-            <ul class="nav navbar-nav">
-            <li><a href="viewermfs.php">HOME</a></li>
-              <li id="highlightbox"><a href="recm.php">RECORDS</a></li>
-              <li><a href="toolsm.php">TOOLS</a></li>
-           </ul>
-            <ul class="nav navbar-nav pull-right">
-                <li><a><i class="glyphicon glyphicon-user"></i><?php echo" " .strtoupper($_SESSION['username'])."  ";?></a></li>
-                <li><a href='logout.php'>Logout</a></li>                </ul>    
+    <meta charset="utf-8"/>    
+    <meta name="viewport" content="width=device-width, initial-scale=1" />      
+    <link rel="shortcut icon" href="//www.ibm.com/favicon.ico" />
+    <meta name="geo.country" content="US" />  
+    <title>Records</title>
+    
+    <script src="//1.www.s81c.com/common/stats/ida_stats.js"></script>
+    <link href="//1.www.s81c.com/common/v18/css/www.css" rel="stylesheet" />
+    <script src="//1.www.s81c.com/common/v18/js/www.js"></script>
+    <link href="//1.www.s81c.com/common/v18/css/forms.css" rel="stylesheet">
+<script src="//1.www.s81c.com/common/v18/js/forms.js"></script>
+    <link href="https://1.www.s81c.com/common/v18/css/tables.css" rel="stylesheet">
+    <script src="https://1.www.s81c.com/common/v18/js/tables.js"></script>
+    <script script type="text/javascript" src="js/filters2.js"></script>
+  <link href="https://1.www.s81c.com/common/v18/css/grid-fluid.css" rel="stylesheet">
+  <script>
+    IBMCore.common.util.config.set({
+       backtotop: {
+        enabled: true
+        }
+    });
+</script>   
+  </head>
+  <body id="ibm-com" class="ibm-type">
+    <div id="ibm-top" class="ibm-landing-page">
+<nav role="navigation" aria-label="NAV">
+        <div class="ibm-sitenav-menu-container">
+            <div class="ibm-sitenav-menu-name">
+                <div id="ibm-home"><a href="viewermfs.php">IBM®</a></div>
+                <a>&nbsp;&nbsp;&nbsp;&nbsp;CTRLSYSTEM</a></div>
+            <div class="ibm-sitenav-menu-list">
+                <ul role="menubar">
+
+                    <li role="presentation"><a role="menuitem"  href="viewermfs.php">Ctrl of Assets</a></li>
+                    <li role="presentation" class="ibm-highlight"><a role="menuitem" href="recm.php">Records</a></li>
+                    
+                  
+                    <!-- Optional right side CTA link -->
+                    <li class="ibm-sitenav-menu-item-right">
+                      <p class="ibm-ind-link ibm-icononly ibm-icononly" style="margin-top: 7px;"><a class="ibm-profile-link"></a></p>
+                      <ul role="menu" style="margin-top: -15px;">
+                            <li role="presentation"><a role="menuitem"><?php echo" " .strtoupper($_SESSION['username'])."  ";?></a></li>
+                            <li role="presentation"><a role="menuitem" href="logout.php">Logout</a></li>
+                        </ul>
+                        
+                      </li>
+                </ul>
+            </div>
+             
         </div>
     </nav>
-</div><!--ENDNAVMENU-->
+        <!--filters-->      
+        <main role="main" aria-labelledby="ibm-pagetitle-h1">
+          <div id="ibm-pcon">
+            <div id="ibm-content">
+              <div id="ibm-content-body">
+                <div id="ibm-content-main">                                   
+<div class="ibm-columns ibm-seamless ibm-padding-bottom-0" data-widget="setsameheight" data-items=".ibm-blocklink">
+    <div class="ibm-col-1-1 ibm-nospacing">
+        <form action="#" class="ibm-row-form">
+          <p>
+                    <input type="text" size="25" placeholder="Serial Number" id="Input1" autofocus>
+          &nbsp;&nbsp;&nbsp;
+                <select id="Input3" title="SEARCH FOR AREA">
+                <option value="" selected="selected">Area</option>
+   <?php
+        require_once('connectsys.php');
+        $query = "SELECT AREA FROM CTRLSYSTEM.INV GROUP BY AREA";
+        $stmt = db2_prepare($db2, $query);
+        if($stmt){
+           $result = db2_execute($stmt);
+           if (!$result) {
+                echo "exec errormsg: " .db2_stmt_errormsg($stmt);
+                exit;
+           }
 
-<!--FILTERS-->
-<form action="#">
-    <div class="row" id="FILTERSBOX">
-    	<div class="col-sm-2">
-    		</label><input type="text" id="Input1" placeholder="S/N" title="SEARCH FOR S/N" style="font-size: 9px;" autofocus>
-            <br>
-            <br>
-            <br>
-           <select id="Input2" placeholder="TYPE" title="SEARCH FOR TYPE" style="font-size: 9px;">
-                <option value="">TYPE</option>
-                <?php
-                    require_once('connectsys.php');
-                    $query = "SELECT TYPE FROM CTRLSYSTEM.RECMFS GROUP BY TYPE";
+         while($row = db2_fetch_array($stmt)){
+                echo '<option value="'. $row[0] .'">'. $row[0] .'</option>';
+          }   
+         }
+    ?>
+</select>
+  &nbsp;&nbsp;&nbsp;&nbsp;
+<select id="Input5" title="SEARCH FOR RESPONSIBLE">
+  <option value="" selected="selected">Responsible</option>
+  <?php
+                 require_once('connectsys.php');
+                    $query = "SELECT RESPONSIBLE FROM CTRLSYSTEM.INV GROUP BY RESPONSIBLE";
                     $stmt = db2_prepare($db2, $query);
                     if($stmt){
                         $result = db2_execute($stmt);
@@ -76,16 +114,19 @@ if($div != "MFS"){
                     echo '<option value="'. $row[0] .'">'. $row[0] .'</option>';
                     }   
                 }
-                    ?>
-                    </select>
-        </div>
-        <div class="col-sm-2">
-            
-             <select id="Input3" placeholder="AREA" title="SEARCH FOR AREA" style="font-size: 9px;">
-             <option value="">AREA</option>
-                                <?php
+            ?>
+</select>
+            </p>
+        </form>
+    </div>
+    <div class="ibm-columns">
+    <div class="ibm-col-6-4  ibm-nospacing">
+        <form class="ibm-row-form">
+<select id="Input2" title="SEARCH FOR TYPE">
+  <option value="" selected="selected">Type</option>
+  <?php
                     require_once('connectsys.php');
-                    $query = "SELECT AREA FROM CTRLSYSTEM.RECMFS GROUP BY AREA";
+                    $query = "SELECT TYPE FROM CTRLSYSTEM.INV GROUP BY TYPE";
                     $stmt = db2_prepare($db2, $query);
                     if($stmt){
                         $result = db2_execute($stmt);
@@ -93,23 +134,18 @@ if($div != "MFS"){
                              echo "exec errormsg: " .db2_stmt_errormsg($stmt);
                             exit;
                           }
-
                 while($row = db2_fetch_array($stmt)){
                     echo '<option value="'. $row[0] .'">'. $row[0] .'</option>';
                     }   
                 }
-                    ?>
-                </select>
-            <br>
-            <br>
-            <br>
-           
-<select id="Input4" placeholder="BRAND" title="SEARCH FOR BRAND" style="font-size: 9px;">
-           
-             <option value="">BRAND</option>
-                  <?php
+    ?>
+</select>
+ &nbsp;&nbsp;&nbsp;&nbsp;
+<select  id="Input4" title="SEARCH FOR BRAND">
+  <option value="" selected="selected">Brand</option>
+ <?php
                     require_once('connectsys.php');
-                    $query = "SELECT BRAND FROM CTRLSYSTEM.RECMFS GROUP BY BRAND";
+                    $query = "SELECT BRAND FROM CTRLSYSTEM.INV GROUP BY BRAND";
                     $stmt = db2_prepare($db2, $query);
                     if($stmt){
                         $result = db2_execute($stmt);
@@ -124,17 +160,12 @@ if($div != "MFS"){
                 }
                     ?>
 </select>
-        </div>
-
-        <div class="col-sm-2">
-        
-
-<select  id="Input5" placeholder="LOCATION" title="SEARCH FOR LOCATION" style="font-size: 9px;">
-
- <option value="">LOCATION</option>
-                    <?php
+ &nbsp;&nbsp;&nbsp;&nbsp;
+<select id="Input6" title="SEARCH FOR COST CENTER">
+  <option value="" selected="selected">Cost Center</option>
+    <?php
                     require_once('connectsys.php');
-                    $query = "SELECT LOCATION FROM CTRLSYSTEM.RECMFS GROUP BY LOCATION";
+                    $query = 'SELECT "COST CENTER" FROM CTRLSYSTEM.INV GROUP BY "COST CENTER"';
                     $stmt = db2_prepare($db2, $query);
                     if($stmt){
                         $result = db2_execute($stmt);
@@ -142,71 +173,45 @@ if($div != "MFS"){
                              echo "exec errormsg: " .db2_stmt_errormsg($stmt);
                             exit;
                           }
-
                 while($row = db2_fetch_array($stmt)){
                     echo '<option value="'. $row[0] .'">'. $row[0] .'</option>';
                     }   
                 }
-                    ?>
+            ?>
 </select>
-
-            <br>
-            <br>
-            <br>
-
-<select  id="Input6" placeholder="ING" title="SEARCH FOR ING." style="font-size: 9px;">
-            <option value="">ING RESPO.</option>
-            <option value="ING1">ING1</option>
-            <option value="ING2">ING2</option>
-            <option value="ING3">ING3</option>
-            <option value="N/A">N/A</option>
-        </select>
-        </div>
-        <div class="col-sm-2">
-        </label>
-<select  id="Input7" placeholder="DATE YY/MM/DD" title="SEARCH FOR DATE" style="font-size: 9px;">
-
- <option value="">DATE YY/MM/DD</option>
-                     <?php
-                    require_once('connectsys.php');
-                    $query = "SELECT DATE FROM CTRLSYSTEM.RECMFS GROUP BY DATE";
-                    $stmt = db2_prepare($db2, $query);
-                    if($stmt){
-                        $result = db2_execute($stmt);
-                        if (!$result) {
-                             echo "exec errormsg: " .db2_stmt_errormsg($stmt);
-                            exit;
-                          }
-
-                while($row = db2_fetch_array($stmt)){
-                    echo '<option value="'. $row[0] .'">'. $row[0] .'</option>';
-                    }   
-                }
-                    ?>
+ &nbsp;&nbsp;&nbsp;&nbsp;
+<select id="Input7" title="SEARCH FOR STATUS AS=ASIGNADO, AL=ALMACEN">
+  <option value="" selected="selected">Status</option>
+   <option value="AS">Asignado</option>
+   <option value="AL">Almacenado</option>
 </select>
-
-        <br>
-        <br>
-         </div>
-         <div class="col-sm-3">
-         <p align="center"><button onClick="window.location.reload()" id="submitbutton" title="Refresh all the web Page and update sql"><i class="glyphicon glyphicon-refresh"></i>REFRESH</button><button onclick="clear(this.form);"  id="submitbutton" type="reset" title="Clear filters"><i class="glyphicon glyphicon-trash"></i>Clear</button>    <button onclick="filter()"  id="submitbutton" title="Execute filters"><i class="glyphicon glyphicon-search"></i>Submit</button></p> 
-          <!--COUNTER-->
-<div class="row">
-    <div class="col-sm-5"></div>
-    <div class="col-sm-1" style=" color:rgb(204,204,204);">Items:<p id="con"></p></div>
-
-</div><!--COUNTER--> 
-    	</div>
-    </div></form><!--ENDFILTERS-->
-
-<!--CONTAINER-->
-<div class="row" id="tablecontainer">
-	<div class="col-sm-12">
-		<table class="table table-bordered" id="myTable">
-			<thead>
-				<tr>
-                	
-					<th>LOCATION</th>
+ &nbsp;&nbsp;&nbsp;&nbsp;
+<select id="Input8" title="SEARCH FOR ING RESPONSIBLE">
+  <option value="" selected="selected">Ing</option>
+  <option value="N/A">N/A</option>
+  <option value="ING1">ING1</option>
+  <option value="ING2">ING2</option>
+  <option value="ING3">ING3</option>
+</select>
+</div> 
+<div class="ibm-col-6-2  ibm-nospacing">
+    <span class="ibm-ind-link ibm-btn-row"><a class="ibm-refresh-link ibm-btn-sec ibm-btn-blue-50" href="#" onClick="window.location.reload()">Refresh</a><a class="ibm-search-link ibm-btn-sec ibm-btn-blue-50" href="#" onclick="filter2()">Search</a><a class="ibm-calculator-link ibm-btn-sec ibm-btn-blue-50">Items:<b id="con"></b></a></span>
+</div>
+</form>
+</div>
+<!--ENDFILTERS-->
+<br>
+<br>
+<br>
+<br>
+<!--CONTENT-->
+<div class="ibm-columns ibm-seamless ibm-padding-bottom-0" data-widget="setsameheight" data-items=".ibm-blocklink">
+    <div class="ibm-col-1-1 ibm-nospacing">
+    <table class="ibm-data-table ibm-grid ibm-altrows  ibm-padding-small ibm-center" id="myTable" data-scrollaxis="x" cellspacing="0" cellpadding="0" border="0" style="font-size: 17px;">
+         <thead>
+        <tr>
+                  
+          <th>LOCATION</th>
                     <th>AREA</th>
                     <th>BRAND</th>
                     <th>TYPE</th>
@@ -216,19 +221,22 @@ if($div != "MFS"){
                     <th>ING</th>
                     <th>DATE</th>
                     <th>DESCRIPTION</th>
-				</tr>
-			</thead>
+        </tr>
+      </thead>
+            <tbody>
             <?php
-            $PHYS = '<i class="glyphicon glyphicon-ok"></i>';
-
+            require_once('connectsys.php');
+            
             require_once('connectsys.php');
             $query = 'SELECT "LOCATION", "AREA", "BRAND", "TYPE", "SN","MT","MODEL","ING", "DATE", "DIVISION", "DESCRI" FROM CTRLSYSTEM.RECMFS';
             $stmt=db2_prepare($db2,$query);
-			if($stmt){  
-            $result=db2_execute($stmt);
-            if(!$result){
-                echo "Error Messange".db2_stmt_errormsg($stmt);
-            }      
+             $stmt = db2_prepare($db2, $query); 
+            if($stmt){     
+                        $result = db2_execute($stmt);
+                        if (!$result) {
+                             echo "exec errormsg: " .db2_stmt_errormsg($stmt);
+                            exit;
+                          }   
         while($row = db2_fetch_array($stmt)){
             echo '<tr><td align="center">' .
             $row[0] . '</td><td align="center">' .
@@ -241,17 +249,41 @@ if($div != "MFS"){
             $row[7] . '</td><td align="center">' .
             $row[8] . '</td><td align="center">' .
             $row[10] . '</td>';
-            }	
+            }   
             echo '</table>';
         }
-            db2_close($db2);
-                    
+            db2_close($db2);  
             
             ?>
-             <a href="#" class="scrollup"><p align="center"><button class="btn btn-default btn-lg" style="background-color:rgb(150,150,150);"><i class="glyphicon glyphicon-chevron-up"></i></button></p></a>
-		</table>
-	</div>
-</div><!--ENDCONATINER-->
+            </tbody>
+        </table>
+    </div>
+</div>
 
-</body>
+
+
+              </div>
+            </div>
+          </div>
+        </main>
+<script>
+function modi(id){
+    win2 = window.open('mod.php?id='+id,'Modifica_U','scrollbars=no,top=220,left=500,width=580,height=450');    
+}
+</script>
+  </body>
+    <br>
+  <br>
+  <br>
+<div class="ibm-columns">
+  <div class="ibm-col-12-9"></div>
+  <div class="ibm-col-12-3 ibm-right">
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <span class="ibm-ind-link"><a class="ibm-email-link ibm-btn-sec ibm-btn-blue-50" href="#" onclick="IBMCore.common.widget.overlay.show('overlayExampleSmall'); return false;">Contact</a> &nbsp;<a class="ibm-help-link ibm-btn-sec ibm-btn-blue-50" href="#">Help/Manual</a></span>
+  <div class="ibm-common-overlay  ibm-overlay-alt" data-widget="overlay" id="overlayExampleSmall">
+       <p class="ibm-center ibm-ind-link""><a href="#" class="ibm-admin-link">alexr@mx1.ibm.com. Alejandro Romero Aldrete</a></p>
+       <p class="ibm-center ibm-ind-link""><a href="#" class="ibm-admin-link">gilbusta@mx1.ibm.com Gilberto Bustamante Sanchez</a></p>
+</div>
+</div>
+</div>
 </html>
