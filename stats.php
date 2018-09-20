4202df658bc0badaf4e5cbdaa4557d5dee1c065b
Alejandro Romero Aldrete <alejandro_romero_aldrete@hotmail.com>
2018-09-20 08:27:00 -0500

    fixes

--- stats.php
+++ stats.php
@@ -19,7 +19,7 @@ if($div != "IT"){
     <meta name="viewport" content="width=device-width, initial-scale=1" />      
     <link rel="shortcut icon" href="//www.ibm.com/favicon.ico" />
     <meta name="geo.country" content="US" />  
-    <title>Delete Record's</title>
+    <title>PC status</title>
     
     <script src="//1.www.s81c.com/common/stats/ida_stats.js"></script>
     <link href="//1.www.s81c.com/common/v18/css/www.css" rel="stylesheet" />
@@ -33,43 +33,29 @@ if($div != "IT"){
   </head>
   <body id="ibm-com" class="ibm-type">
     <div id="ibm-top" class="ibm-landing-page">
-        <main role="main" aria-labelledby="ibm-pagetitle-h1">
-                    <form action="#" method="post" class="ibm-row-form">          
+        <main role="main" aria-labelledby="ibm-pagetitle-h1">      
         <div class="ibm-columns ibm-seamless ibm-padding-bottom-0 ibm-pull-quote ibm-h1">
             <div class="ibm-col-6-6" id="1">
-               <p class=" ibm-h1 ibm-center">ARE YOU SURE THAT WANT TO DELETE THE RECORD TABLE? (YOU MUST TO BACKUP THE INFO. BEFORE YOU DELETE IT)</p>
+               <p class=" ibm-h1 ibm-center">PC STATUS SOFTWARE.</p>
+                </div>
+        </div>
+        <div class="ibm-columns ibm-seamless ibm-padding-bottom-0 ibm-pull-quote ibm-h1">
+            <div class="ibm-col-6-6" id="1">
+               <p class=" ibm-h3 ibm-center">Used for check the conexion of a MFG PC from Storage and Appliance.</p>
+                </div>
+        </div>
+         <div class="ibm-columns ibm-seamless ibm-padding-bottom-0 ibm-pull-quote ibm-h1">
+            <div class="ibm-col-6-6" id="1">
+               <p class="ibm-center">Make sure you have the most recent JRE from JAVA.<br>At the time you execute the program wait around 3 minutes for display the information, the software call every pc through the network.</p>
                 </div>
         </div>
         <div class="ibm-columns ibm-center">
             <div class="ibm-col-1-1" id="2">
-              <button type="submit" class="ibm-ind-link ibm-btn-sec ibm-btn-transparent ibm-btn-small"><a class="ibm-confirm-link  ibm-btn-sec" href="#">Yes</a></button>
+              <button type="submit" class="ibm-ind-link ibm-btn-sec ibm-btn-transparent ibm-btn-small"><a class="ibm-confirm-link  ibm-btn-sec" href="PCstats.jar">Yes</a></button>
                 <b class="ibm-ind-link"><a class="ibm-close-link  ibm-btn-pri" href="javascript:close()">No</a></b>
             </div>
         </div> 
-    </form>     
-    <?php 
-            if(isset($_POST['submit'])){
-              require_once('connectsys.php');
-              $tru = "TRUNCATE CTRLSYSTEM.REC IMMEDIATE";
-              $stmt = db2_prepare($db2, $tru);
-              if($stmt){
-                $result = db2_execute($stmt);
-                if (!$result) {
-                  echo "exec errormsg: " .db2_stmt_errormsg($stmt);
-                  exit;
-                } 
-            }
-                 echo '<script>$("#" + 1).remove();</script>';
-                 echo '<script>$("#" + 2).remove();</script>';
-
-               echo '<div class="ibm-col-6-6">
-               <p class=" ibm-h1 ibm-center">RECORDS REMOVED</p>
-                </div>
-                   <div class="ibm-col-6-6">
-              <b class="ibm-ind-link"><a class="ibm-confirm-link  ibm-btn-pri" href="javascript:close()">Close Window</a></b>
-                </div>';
-              }
-              ?>
+    
         </main>
       </div>
     </div>
