34c61904dfe337548a955ff91d3a14ccbe9bed11
Gilberto <riuku530@gmail.com>
2018-10-28 20:02:47 -0600

    conect

--- connectsys.php
+++ connectsys.php
@@ -1,8 +1,12 @@
 <?php
 /*Desarrollado por Alejandro Romero Aldrete*/
-    $details  = json_decode( getenv( "VCAP_SERVICES" ), true );
+   $details  = json_decode( getenv( "VCAP_SERVICES" ), true );
     $dsn      = $details [ "user-provided" ][0][ "credentials" ][ "dsn" ];
+    
     $driver = "DRIVER={IBM DB2 ODBC DRIVER};";
+    
+   
+
     $conn_string = $driver . $dsn;     # Non-SSL
     $db2 = db2_pconnect( $conn_string, "", "" );
-?>
+?> 
